ef5a3fa678f402fff73b823b6156564e3dccbb7b
dhikrama <dhikrama.creator@gmail.com>
2025-04-30 20:58:31 +0700

    Prubahan mendasar pada hsts

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"time"
 
 	m "github/dhikrama/go/src"
 	r "github/dhikrama/go/src/routes"
@@ -21,37 +20,25 @@ func main() {
 	})
 
 	app.Use(helmet.New(helmet.Config{
-		HSTSMaxAge:            300,
-		HSTSExcludeSubdomains: true,
-		XSSProtection:         "0",
-		XFrameOptions:         "ALLOW-FROM https://maunguli.com",
+		HSTSMaxAge:                300,
+		HSTSExcludeSubdomains:     true,
+		XSSProtection:             "0",
+		XFrameOptions:             "SAMEORIGIN",
+		ContentSecurityPolicy:     "same-origin",
+		CrossOriginOpenerPolicy:   "same-origin",
+		CrossOriginEmbedderPolicy: "require-corp",
 	}))
 	app.Static("/", "./static")
 
 	r.Index(app)
-
 	m.FormContact(app)
-
-	// Custom 404 Handler
-	app.Use(func(c *fiber.Ctx) error {
-		return c.Status(404).Render("404", fiber.Map{
-			"Title": "404 Not Found",
-			"Year":  time.Now().Year(),
-		}, "layouts/main") // Render the 404 template
-	})
+	r.Erorr404(app)
 
 	app.Get("/layout", func(c *fiber.Ctx) error {
 		// Render index within layouts/main
 		return c.Render("index", fiber.Map{
 			"Title": "Maunguli jasa tukang bangunan",
 		}, "layouts/main")
-	})
-
-	app.Get("/layouts-nested", func(c *fiber.Ctx) error {
-		// Render index within layouts/nested/main within layouts/nested/base
-		return c.Render("index", fiber.Map{
-			"Title": "Hello, World!",
-		}, "layouts/nested/main", "layouts/nested/base")
 	})
 
 	log.Fatal(app.Listen(":8080"))
---
+++ src/routes/error_404.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func Erorr404(app *fiber.App) {
+	// Custom 404 Handler
+	app.Use(func(c *fiber.Ctx) error {
+		return c.Status(404).Render("404", fiber.Map{
+			"Title": "404 Not Found",
+			"Year":  time.Now().Year(),
+		}, "layouts/main") // Render the 404 template
+	})
+}
